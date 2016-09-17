ae77342af7b0d877e5ddd61dd4890f0b1e93cbda
Carmine Paolino <carmine@paolino.me>
2016-09-17 18:45:19 +0200

    better pwd

--- segments/pwd.fish
+++ segments/pwd.fish
@@ -5,15 +5,13 @@ function FLSEG_PWD
 
 	set -l pwd (prompt_pwd)
 
-	if echo $pwd  | grep '^~' >>  /dev/null
-		FLINT_CLOSE $FLCLR_PWD_BG_HOME $FLCLR_PWD_FG_HOME
-		printf "~"
+	FLINT_CLOSE $FLCLR_PWD_BG_HOME $FLCLR_PWD_FG_HOME
+
+	if echo $pwd  | grep '^/' >> /dev/null
+		printf '/'
 	end
 
-	if not echo $pwd  | grep '^~$' >>  /dev/null
-		set -l sep (echo $FLSYM_SEPARATOR | sed 's,\\\\,\\\\\\\\,g')
-		FLINT_CLOSE $FLCLR_PWD_BG $FLCLR_PWD_FG
-		printf (printf $pwd | sed "s,~,,; s,/,,; s,/,$sep,g; s,^\$,/,")
-	end
+	set -l sep (echo $FLSYM_SEPARATOR | sed 's,\\\\,\\\\\\\\,g')
+	printf (printf $pwd | sed "s,/,$sep,g; s,^\$,/,")
 
 end
