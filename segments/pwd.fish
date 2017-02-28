73e4fbda27844a751257fc6e347bbff18dabdf5f
Carmine Paolino <carmine@paolino.me>
2017-02-28 11:22:13 +0100

    Fix bugs with pwd and /

--- segments/pwd.fish
+++ segments/pwd.fish
@@ -2,16 +2,7 @@
 # -*-  mode:fish; tab-width:4  -*-
 
 function FLSEG_PWD
-
-	set -l pwd (prompt_pwd)
-
 	FLINT_CLOSE $FLCLR_PWD_BG_HOME $FLCLR_PWD_FG_HOME
 
-	if echo $pwd  | grep '^/' >> /dev/null
-		printf '/'
-	end
-
-	set -l sep (echo $FLSYM_SEPARATOR | sed 's,\\\\,\\\\\\\\,g')
-	printf (printf $pwd | sed "s,/,$sep,g; s,^\$,/,")
-
+    printf (prompt_pwd | string replace -r -a '(?!^)/' $FLSYM_SEPARATOR)
 end
